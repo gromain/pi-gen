8e347242a2345c647fb2967c95bf0d0d6c7137c1
jancelin <julien.ancelin@inra.fr>
2020-05-15 17:16:29 +0200

    add cloud-init

--- note_JA.sh
+++ note_JA.sh
@@ -5,11 +5,16 @@ cd pi-gen
 find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 #make lite img
 
-touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
+touch ./stage4/SKIP ./stage5/SKIP
 touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
 
-sudo docker run -it --privileged --volumes-from=pigen_work pi-gen /bin/bash
+sudo ./build-docker.sh 
 sudo CONTINUE=1 ./build-docker.sh
-PRESERVE_CONTAINER=1 ./build-docker.sh
 
+sudo PRESERVE_CONTAINER=1 ./build-docker.sh
+
+sudo PRESERVE_CONTAINER=1 CONTINUE=1 ./build-docker.sh
+
+
+sudo docker run -it --privileged --volumes-from=pigen_work pi-gen /bin/bash
