4b5638f62a54e44791f3a9933bf53f01d61cecc9
jancelin <julien.ancelin@inra.fr>
2020-05-19 15:22:10 +0200

    upd install.sh rtklib

---
+++ stage4/00-rtkbase/01-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+install -m 644 files/install.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+install -m 644 files/sed.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+
+on_chroot << EOF
+cd /home/centipede
+chmod +x sed.sh
+./sed.sh
+EOF
+
+on_chroot << EOF
+cd /home/centipede
+chmod +x install.sh
+echo '################################'
+echo 'START INSTALL'
+echo '################################'
+./install.sh --dependencies --rtklib --rtkbase_release
+echo '################################'
+echo 'INSTALL FINISH'
+echo '################################'
+EOF
