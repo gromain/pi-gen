08d313329f05537f64a71fbb83050080859eb0f8
jancelin <julien.ancelin@inra.fr>
2020-05-25 16:04:14 +0200

    upd install

--- stage3/01-pystemd/01-run.sh
+++ stage3/01-pystemd/01-run.sh
@@ -3,7 +3,8 @@
 on_chroot << EOF
 echo 'get pystemd'
 cd /home/centipede
-if cd pystemd; then git pull; else git clone -b master https://github.com/facebookincubator/pystemd.git; fi
+if [ -d pystemd ]; then git -C pystemd pull; else git clone -b master https://github.com/facebookincubator/pystemd.git; fi
+cd pystemd
 python3 -m pip install -r requirements.txt
 python3 -m pip install --upgrade cython
 python3 setup.py install
--- stage3/02-rtkbase/01-run.sh
+++ stage3/02-rtkbase/01-run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -e
 
-install -m 644 files/install.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+#install -m 644 files/install.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
 install -m 644 files/sed.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
 install -m 644 files/unsed.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
 
 on_chroot << EOF
 cd /home/centipede
-chmod +x sed.sh
-chmod +x unsed.sh
+wget https://raw.githubusercontent.com/stefal/rtkbase/web_gui/tools/install.sh
+find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 ./sed.sh
 EOF
 
 on_chroot << EOF
 cd /home/centipede
-chmod +x install.sh
 echo '################################'
 echo 'START INSTALL'
 echo '################################'
