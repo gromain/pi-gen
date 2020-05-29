1e668ecffa46e0f26968500d5b049370d7171584
jancelin <julien.ancelin@inra.fr>
2020-05-29 11:22:00 +0200

    root to user for services

--- stage3/01-rtkbase/files/sed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-_user="${PWD##*/}"
-cd /home/$_user
-sed -i -e "s/\$(logname)/\$_user/g" /home/$_user/install.sh
-sed -i -e "s/\$(logname)/\$_user/g" /home/$_user/rtkbase/copy_unit.sh 
-
--- stage3/01-rtkbase/files/unsed.sh
+++ stage3/01-rtkbase/files/unsed.sh
@@ -2,5 +2,5 @@
 set -x
 _user="${PWD##*/}"
 cd /home/$_user
-sed -i -e "s/\$_user/\$(logname)/g" /home/basegnss/install.sh
-sed -i -e "s/\$_user/\$(logname)/g" /home/$_user/rtkbase/copy_unit.sh
+sed -i -e "s/$_user/\$(logname)/g" /home/$_user/install.sh
+sed -i -e "s/$_user/\$(logname)/g" /home/$_user/rtkbase/copy_unit.sh
