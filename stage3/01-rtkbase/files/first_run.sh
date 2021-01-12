f282c84ec0d93e41b15e2d8bfa30326fb9680ff8
jancelin <julien.ancelin@inra.fr>
2021-01-12 09:28:24 +0100

    rtkbase dev branch

--- stage3/01-rtkbase/files/first_run.sh
+++ stage3/01-rtkbase/files/first_run.sh
@@ -3,7 +3,8 @@
 _user="${PWD##*/}"
 cd /home/$_user
 find ./ -type f -iname "install.sh*" -exec rm {} \;
-wget https://raw.githubusercontent.com/stefal/rtkbase/master/tools/install.sh
+wget https://raw.githubusercontent.com/stefal/rtkbase/dev/tools/install.sh
+#wget https://raw.githubusercontent.com/stefal/rtkbase/master/tools/install.sh
 find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 echo '#####MODIFY $(logname) install.sh########'
 sed -i -e "s/\$(logname)/$_user/g" /home/$_user/install.sh
