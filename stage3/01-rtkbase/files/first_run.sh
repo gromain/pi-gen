ce8f408319cdc069ed6d900000349c8e1273dd19
jancelin <julien.ancelin@inra.fr>
2021-01-15 09:56:16 +0100

    upd install rtkbase 2.2.0 serial not open!

--- stage3/01-rtkbase/files/first_run.sh
+++ stage3/01-rtkbase/files/first_run.sh
@@ -3,12 +3,17 @@
 _user="${PWD##*/}"
 cd /home/$_user
 find ./ -type f -iname "install.sh*" -exec rm {} \;
-wget https://raw.githubusercontent.com/stefal/rtkbase/dev/tools/install.sh
-#wget https://raw.githubusercontent.com/stefal/rtkbase/master/tools/install.sh
+#wget https://raw.githubusercontent.com/stefal/rtkbase/dev/tools/install.sh
+wget https://raw.githubusercontent.com/stefal/rtkbase/master/tools/install.sh
 find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 echo '#####MODIFY $(logname) install.sh########'
 sed -i -e "s/\$(logname)/$_user/g" /home/$_user/install.sh
 echo '#####DONE !##############################'
+
+#if rtkbase dev branch
+#sed -i '/rtkbase.git/a cd rtkbase && git checkout dev && cd ..' ./install.sh
+
 cat install.sh
+#./install.sh --dependencies --rtklib --rtkbase-repo --gpsd-chrony
 ./install.sh --dependencies --rtklib --rtkbase-release --gpsd-chrony
 find ./ -type f -iname "*.sh" -exec chmod +x {} \;
